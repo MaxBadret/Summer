2274a586ab85381fb7783054bf65b315c5edc5f3
Stealthmefie <stealthmefi@gmail.com>
2025-07-09 15:52:09 +0500

    Немного кода

---
+++ Assets/Scripts/Components/ResistorComponent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ResistorComponent : BaseComponent
+{
+    [SerializeField] private float resistance = 10f;
+
+    public override float GetResistance() => resistance;
+
+    public override void ProcessSignal(SignalData input)
+    {
+        inputSignal = input;
+
+        float voltageDrop = input.Current * resistance;
+        float outVoltage = input.Voltage - voltageDrop;
+
+        outputSignal = new SignalData(outVoltage, input.Current);
+    }
+}
+
